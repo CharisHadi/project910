25256eb186aed3ff6b177b1d3da8779d815e09f2
CharisHadi <charishadi@gmail.com>
2019-09-12 20:03:46 -0700

    Run BUild

---
+++ client/build/precache-manifest.3f541dc04fcecd41953f19814e9b78b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a5d29e8a25b7722cb09c",
+    "url": "/static/js/main.65082303.chunk.js"
+  },
+  {
+    "revision": "89907e73fad5008c7792",
+    "url": "/static/js/2.ff9e53b3.chunk.js"
+  },
+  {
+    "revision": "a5d29e8a25b7722cb09c",
+    "url": "/static/css/main.a1fe2ea7.chunk.css"
+  },
+  {
+    "revision": "89907e73fad5008c7792",
+    "url": "/static/css/2.cd25d2ff.chunk.css"
+  },
+  {
+    "revision": "4c2b17aecfdd38333e8edd3cb0492c38",
+    "url": "/index.html"
+  }
+];
